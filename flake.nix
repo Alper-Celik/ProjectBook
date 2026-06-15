9e067a1fb654fc2805d527ffdfc3ba05da1b4491
Alper Çelik <alper@alper-celik.dev>
2026-06-16 02:02:28 +0300

    some prep

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              dotnet-sdk_10
+              nodejs_22
+            ];
+          };
+        };
+    };
+}
